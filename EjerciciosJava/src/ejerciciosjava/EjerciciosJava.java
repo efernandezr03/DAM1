dfdc8732da66cd9e8d6c66c7ba1d07683447f4dd
efernandezr03 <efernandezr03@aula1-05>
2015-10-06 20:07:39 +0200

    subo mi primer ej en java

---
+++ EjerciciosJava/src/ejerciciosjava/EjerciciosJava.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciosjava;
+
+/**
+ *
+ * @author efernandezr03
+ */
+public class EjerciciosJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         int a,b,c;
+         
+         a = b = 20;
+         c=a+b;
+         System.out.println(c);
+    }
+    
+}
